466e6a0cb50e7648a971f1e08c4853d132391d14
veypi <veypi@qq.com>
2021-10-30 14:34:54 +0800

    bug

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import { App } from 'vue'
+import {App} from 'vue'
 import OneIcon from './main.vue'
 
 function loadJS(url: string) {
-  const script = document.createElement('script')
-  script.type = 'text/javascript'
-  script.src = url
-  document.getElementsByTagName('head')[0].appendChild(script)
+    const script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.src = url
+    document.getElementsByTagName('head')[0].appendChild(script)
 }
 
-export OneIcon
+export {OneIcon}
 
 export default {
-  installed: false,
-  install(vue: App, options?: { href: '' }): void {
-    if (this.installed) {
-      return
+    installed: false,
+    install(vue: App, options?: { href: '' }): void {
+        if (this.installed) {
+            return
+        }
+        this.installed = true
+        if (options && options.href) {
+            loadJS(options.href)
+        }
+        vue.component('one-icon', OneIcon)
     }
-    this.installed = true
-    if (options && options.href) {
-      loadJS(options.href)
-    }
-    vue.component('one-icon', OneIcon)
-  }
 }
